c38e443112211374be30ddfd59c83b4402054a88
김멍몽 <wjdahrrla@gmail.com>
2024-02-07 23:11:03 +0900

    feat: 친구 엔티티 수정

--- src/main/java/universe/universe/entitiy/friend/Friend.java
+++ src/main/java/universe/universe/entitiy/friend/Friend.java
@@ -1,4 +1,14 @@
 package universe.universe.entitiy.friend;
 
-public class Friend {
+import jakarta.persistence.*;
+import universe.universe.entitiy.user.BaseEntity;
+import universe.universe.entitiy.user.Status;
+
+public class Friend extends BaseEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "friend_id")
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private Status friendStatus;
 }
