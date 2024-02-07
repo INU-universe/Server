8d10b7c526044f46113f6a2f864745b495c8b90d
김멍몽 <wjdahrrla@gmail.com>
2024-02-07 22:00:11 +0900

    init: 프로젝트 생성 및 초기 설정

---
+++ src/main/java/universe/universe/UnIverseApplication.java
@@ -0,0 +1,13 @@
+package universe.universe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnIverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnIverseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/universe/universe/UnIverseApplicationTests.java
@@ -0,0 +1,13 @@
+package universe.universe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnIverseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
