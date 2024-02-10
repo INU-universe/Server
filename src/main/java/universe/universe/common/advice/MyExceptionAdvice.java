dc7eae94439d30a6bc33f7d034ce5a2cbe95d2fb
김멍몽 <wjdahrrla@gmail.com>
2024-02-10 13:35:15 +0900

    fix: MyExceptionAdvice 수정

--- src/main/java/universe/universe/common/advice/MyExceptionAdvice.java
+++ src/main/java/universe/universe/common/advice/MyExceptionAdvice.java
@@ -1,47 +1,44 @@
+package universe.universe.common.advice;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import universe.universe.common.exception.*;
-import universe.universe.common.reponse.Response;
+import universe.universe.dto.response.ResponseDTO;
 
 @Slf4j
 @RestControllerAdvice
 public class MyExceptionAdvice {
     @ExceptionHandler(Exception400.class)
     public ResponseEntity<?> badRequest(Exception400 e){
-        return buildResponse(e.status().value(), e.body(), e.getMessage());
+        return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception401.class)
     public ResponseEntity<?> unAuthorized(Exception401 e){
-        return buildResponse(e.status().value(), e.body(), e.getMessage());
+        return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception403.class)
     public ResponseEntity<?> forbidden(Exception403 e){
-        return buildResponse(e.status().value(), e.body(), e.getMessage());
+        return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception404.class)
     public ResponseEntity<?> notFound(Exception404 e){
-        return buildResponse(e.status().value(), e.body(), e.getMessage());
+        return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception500.class)
     public ResponseEntity<?> serverError(Exception500 e){
-        return buildResponse(e.status().value(), e.body(), e.getMessage());
+        return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> unknownServerError(Exception e){
-        Response<String> response = Response.ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), "unknownServerError", e.getMessage());
-        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    private ResponseEntity<?> buildResponse(int code, Object data, String message) {
-        Response<Object> response = Response.ERROR(code, message, data);
-        return new ResponseEntity<>(response, HttpStatus.valueOf(code));
+        ResponseDTO<String> responseDTO = new ResponseDTO<>(HttpStatus.INTERNAL_SERVER_ERROR, "unknownServerError", e.getMessage());
+        return new ResponseEntity<>(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
