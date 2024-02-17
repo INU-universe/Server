9191dee4815558bfb8ac82c4ec4a0001b972e6b9
김멍몽 <wjdahrrla@gmail.com>
2024-02-17 22:03:17 +0900

    fix: controller에 delete 추가

--- src/main/java/universe/universe/controller/chatRoom/ChatRoomApiController.java
+++ src/main/java/universe/universe/controller/chatRoom/ChatRoomApiController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import universe.universe.common.exception.Exception400;
 import universe.universe.common.exception.Exception500;
 import universe.universe.common.reponse.ApiResponse;
@@ -31,6 +28,20 @@ public class ChatRoomApiController {
             String userEmail = getUserEmail();
             ChatRoomResponseDTO.ChatRoomCreateDTO createChatRoomRelation = chatRoomService.create(userEmail, chatRoomCreateDTO);
             return ResponseEntity.ok().body(ApiResponse.SUCCESS(HttpStatus.CREATED.value(), "채팅방 생성 성공입니다.", createChatRoomRelation));
+        } catch (Exception400 e) {
+            return ResponseEntity.badRequest().body(ApiResponse.FAILURE(e.status().value(), e.getMessage()));
+        } catch (Exception500 e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.ERROR(e.status().value(), e.getMessage()));
+        }
+    }
+
+    @PostMapping("/delete/{chatRoomId}")
+    public ResponseEntity<?> delete(@PathVariable Long chatRoomId) {
+        try {
+            log.info("[ChatRoomApiController]");
+            String userEmail = getUserEmail();
+            chatRoomService.delete(userEmail, chatRoomId);
+            return ResponseEntity.ok().body(ApiResponse.SUCCESS(HttpStatus.CREATED.value(), "채팅방 삭제 성공입니다."));
         } catch (Exception400 e) {
             return ResponseEntity.badRequest().body(ApiResponse.FAILURE(e.status().value(), e.getMessage()));
         } catch (Exception500 e) {
