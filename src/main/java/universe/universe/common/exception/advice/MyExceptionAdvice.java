419acc4f2bdd11ce7417e0f4b788ffa8fd08e727
김멍몽 <wjdahrrla@gmail.com>
2024-02-26 01:18:31 +0900

    fix: OauthApiController

--- src/main/java/universe/universe/common/exception/advice/MyExceptionAdvice.java
+++ src/main/java/universe/universe/common/exception/advice/MyExceptionAdvice.java
@@ -1,4 +1,4 @@
-package universe.universe.common.advice;
+package universe.universe.common.exception.advice;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
---
+++ src/main/java/universe/universe/controller/oauth/OauthApiController.java
@@ -0,0 +1,7 @@
+package universe.universe.controller.oauth;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OauthApiController {
+}
