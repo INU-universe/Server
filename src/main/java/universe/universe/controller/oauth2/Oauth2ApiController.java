f079ddb52d9af3fc07a60bd9f36fb1db57fc58e5
김멍몽 <wjdahrrla@gmail.com>
2024-02-28 00:51:14 +0900

    fix: Oauth2ApiController 수정

--- src/main/java/universe/universe/controller/oauth2/Oauth2ApiController.java
+++ src/main/java/universe/universe/controller/oauth2/Oauth2ApiController.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 import universe.universe.entitiy.user.User;
 import universe.universe.repository.user.UserRepository;
@@ -17,11 +19,19 @@ public class Oauth2ApiController {
     final private UserRepository userRepository;
     final private AuthenticationService authenticationService;
 
-    @PostMapping("/login/oauth2/code/google")
-    public String Oauth2Login() {
-//        String userEmail = getUserEmail();
-        return "gdgdg";
+//    @GetMapping("/login/oauth2/code/google")
+//    public String Oauth2Login() {
+////        String userEmail = getUserEmail();
+//        return "index";
+//
+//    }
 
+    @GetMapping("/login/oauth2/code/google")
+    public @ResponseBody String loginOauthTest(Authentication authentication, @AuthenticationPrincipal OAuth2User oauth) {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        System.out.println("authentication = " + oAuth2User.getName());
+        System.out.println("oauth = " + oauth.getName());
+        return "Oauth 세션 정보 확인하기";
     }
 
     private String getUserEmail() {
