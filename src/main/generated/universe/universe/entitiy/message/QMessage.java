cca701cdbd674fca414a93fb028c8ccadbeecbcc
김멍몽 <wjdahrrla@gmail.com>
2024-03-03 20:23:19 +0900

    fix: gradle 및 java 버전 수정

---
+++ src/main/generated/universe/universe/entitiy/message/QMessage.java
@@ -0,0 +1,64 @@
+package universe.universe.entitiy.message;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QMessage is a Querydsl query type for Message
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMessage extends EntityPathBase<Message> {
+
+    private static final long serialVersionUID = -2106020872L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QMessage message = new QMessage("message");
+
+    public final universe.universe.entitiy.base.QBaseEntity _super = new universe.universe.entitiy.base.QBaseEntity(this);
+
+    public final universe.universe.entitiy.chatRoom.QChatRoom chatRoom;
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdTime = _super.createdTime;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedTime = _super.updatedTime;
+
+    public final universe.universe.entitiy.user.QUser user;
+
+    public QMessage(String variable) {
+        this(Message.class, forVariable(variable), INITS);
+    }
+
+    public QMessage(Path<? extends Message> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QMessage(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QMessage(PathMetadata metadata, PathInits inits) {
+        this(Message.class, metadata, inits);
+    }
+
+    public QMessage(Class<? extends Message> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.chatRoom = inits.isInitialized("chatRoom") ? new universe.universe.entitiy.chatRoom.QChatRoom(forProperty("chatRoom")) : null;
+        this.user = inits.isInitialized("user") ? new universe.universe.entitiy.user.QUser(forProperty("user"), inits.get("user")) : null;
+    }
+
+}
+
---
+++ src/main/generated/universe/universe/entitiy/user/QUser.java
@@ -0,0 +1,73 @@
+package universe.universe.entitiy.user;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QUser is a Querydsl query type for User
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QUser extends EntityPathBase<User> {
+
+    private static final long serialVersionUID = 667347238L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QUser user = new QUser("user");
+
+    public final universe.universe.entitiy.base.QBaseEntity _super = new universe.universe.entitiy.base.QBaseEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdTime = _super.createdTime;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final universe.universe.entitiy.location.QLocation location;
+
+    public final StringPath role = createString("role");
+
+    public final DateTimePath<java.time.LocalDateTime> schoolDate = createDateTime("schoolDate", java.time.LocalDateTime.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedTime = _super.updatedTime;
+
+    public final StringPath userEmail = createString("userEmail");
+
+    public final StringPath userImg = createString("userImg");
+
+    public final StringPath userName = createString("userName");
+
+    public final StringPath userPassword = createString("userPassword");
+
+    public final EnumPath<UserStatus> userStatus = createEnum("userStatus", UserStatus.class);
+
+    public QUser(String variable) {
+        this(User.class, forVariable(variable), INITS);
+    }
+
+    public QUser(Path<? extends User> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QUser(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QUser(PathMetadata metadata, PathInits inits) {
+        this(User.class, metadata, inits);
+    }
+
+    public QUser(Class<? extends User> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.location = inits.isInitialized("location") ? new universe.universe.entitiy.location.QLocation(forProperty("location"), inits.get("location")) : null;
+    }
+
+}
+
