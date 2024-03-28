25a2bda939604cc17a25a69c6234bb5400889d2c
김멍몽 <wjdahrrla@gmail.com>
2024-03-28 23:17:22 +0900

    fix: PrinipalOauth2UserService optional 오류 해결

--- src/main/java/universe/universe/global/auth/oauth2/PrincipalOauth2UserService.java
+++ src/main/java/universe/universe/global/auth/oauth2/PrincipalOauth2UserService.java
@@ -42,7 +42,8 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         log.info("userInfoName: {}", userInfoName);
         log.info("userInfoImg: {}", userInfoImg);
 
-        User user = userRepository.findByUserEmail(userInfoEmail).get();
+        User user = userRepository.findByUserEmail(userInfoEmail)
+                .orElse(null);
 
         if (user != null) {
             log.info("로그인을 이미 했음, 자동 회원가입이 되어있다.");
