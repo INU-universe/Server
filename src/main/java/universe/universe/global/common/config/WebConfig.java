0bccc07798b0ed408245bedff1fc7b8d932ffe8b
김멍몽 <wjdahrrla@gmail.com>
2024-04-02 22:29:19 +0900

    fix: WebConfig 수정

--- src/main/java/universe/universe/global/common/config/WebConfig.java
+++ src/main/java/universe/universe/global/common/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대하여
-                .allowedOrigins("http://localhost:3000") // 리액트 앱의 URL을 허용 (개발 환경일 경우)
+                .allowedOrigins("*") // 리액트 앱의 URL을 허용 (개발 환경일 경우)
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드 지정
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 쿠키를 포함한 요청 허용
