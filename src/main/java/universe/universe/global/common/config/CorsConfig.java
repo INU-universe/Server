ab71ca023d3a4a0f195c9a103c4f2286b43680b4
김멍몽 <wjdahrrla@gmail.com>
2024-04-02 22:50:52 +0900

    fix: cors 에러

--- src/main/java/universe/universe/global/common/config/CorsConfig.java
+++ src/main/java/universe/universe/global/common/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package universe.universe.global.common.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -7,9 +8,11 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
+@Slf4j
 public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
+        log.info("[corsFilter] 입장");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); // 내 서버가 응답을 할 때 json을 자바스크립트에서 처리할 수 있게 할지를 설정하는 것이다.
@@ -18,6 +21,8 @@ public class CorsConfig {
         config.addAllowedMethod("*"); // 모든 post, get, put, delete, patch 요청을 허용하겠다.
 
         source.registerCorsConfiguration("/**", config);
+
+        log.info("[corsFilter] 퇴장");
         return new CorsFilter(source);
     }
 
